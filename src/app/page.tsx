283af130e464454ad47b883024a0ce36f5fe21e9
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-01 20:18:55 +0530

    add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>Pizza app</h1>
-      <Button
-        style={{
-          backgroundColor: "#000",
-          color: "#fff",
-          cursor: "pointer",
-          padding: "5px 10px",
-          borderRadius: "10px",
-        }}
-        variant={"link"}
-      >
-        Press here
-      </Button>
+      <Button>Press here</Button>
     </>
   );
 }
