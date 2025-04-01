3cc0b7e0f238e0c71537c643cdbe6faa2924d429
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-01 20:08:32 +0530

    add shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1>Pizza app</h1>;
+  return (
+    <>
+      <h1>Pizza app</h1>
+      <Button
+        style={{
+          backgroundColor: "#000",
+          color: "#fff",
+          cursor: "pointer",
+          padding: "5px 10px",
+          borderRadius: "10px",
+        }}
+        variant={"link"}
+      >
+        Press here
+      </Button>
+    </>
+  );
 }
