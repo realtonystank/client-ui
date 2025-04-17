599ace55e0f2658155e82b89c1a5973c0794957d
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-17 21:34:46 +0530

    prevent user from accessing register page when logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/login")) {
+  if (
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/register")
+  ) {
     const accessToken = request.cookies.get("accessToken");
     if (accessToken?.value) {
       return NextResponse.redirect(new URL("/", request.url));
