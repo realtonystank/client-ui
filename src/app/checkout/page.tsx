aa9f407d77b99f8bbc9249a7c13a8544691b85ec
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-24 00:36:55 +0530

    create basic checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,49 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+
+const Checkout = async () => {
+  return (
+    <div className="flex container gap-6 mt-16 mx-auto">
+      <Card className="w-3/5 border-none">
+        <CardHeader>
+          <CardTitle>Customer details</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="fname">First Name</Label>
+              <Input
+                id="fname"
+                type="text"
+                className="w-full"
+                defaultValue=""
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="lname">Last Name</Label>
+              <Input
+                id="lname"
+                type="text"
+                className="w-full"
+                defaultValue=""
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="text"
+                className="w-full"
+                defaultValue=""
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Checkout;
