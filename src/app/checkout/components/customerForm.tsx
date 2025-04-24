213846dd4dc6a6003268bdd406f254f9a06f9e55
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-24 21:43:49 +0530

    fetch customer data

--- src/app/checkout/components/customerForm.tsx
+++ src/app/checkout/components/customerForm.tsx
@@ -2,14 +2,40 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { getCustomer } from "@/lib/http/api";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
 import React from "react";
 
+interface Address {
+  text: string;
+  isDefault: boolean;
+}
+
+interface Customer {
+  firstName: string;
+  lastName: string;
+  email: string;
+  addresses: Address[];
+}
+
 const CustomerForm = () => {
+  const { data: customer, isPending } = useQuery({
+    queryKey: ["customer"],
+    queryFn: async (): Promise<Customer> => {
+      const response = await getCustomer();
+      return response.data as Customer;
+    },
+  });
+
   return (
     <div className="flex container gap-6 mt-16 mx-auto">
       <Card className="w-3/5 border-none">
-        <CardHeader>
+        <CardHeader className="flex justify-between min-h-6">
           <CardTitle>Customer details</CardTitle>
+          {isPending && (
+            <Loader className="h-6 w-6 animate-spin text-primary" />
+          )}
         </CardHeader>
         <CardContent>
           <div className="grid gap-6">
@@ -19,7 +45,8 @@ const CustomerForm = () => {
                 id="fname"
                 type="text"
                 className="w-full"
-                defaultValue=""
+                defaultValue={customer?.firstName}
+                disabled={true}
               />
             </div>
             <div className="grid gap-3">
@@ -28,7 +55,8 @@ const CustomerForm = () => {
                 id="lname"
                 type="text"
                 className="w-full"
-                defaultValue=""
+                defaultValue={customer?.lastName}
+                disabled={true}
               />
             </div>
             <div className="grid gap-3">
@@ -37,7 +65,8 @@ const CustomerForm = () => {
                 id="email"
                 type="text"
                 className="w-full"
-                defaultValue=""
+                defaultValue={customer?.email}
+                disabled={true}
               />
             </div>
           </div>
