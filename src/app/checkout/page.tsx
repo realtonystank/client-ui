19b4853f3cfa1750d10339c1e9794927507aef5c
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-24 20:55:31 +0530

    move customer form to client component

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,7 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { getSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 import React from "react";
+import CustomerForm from "./components/customerForm";
 
 const Checkout = async ({
   searchParams,
@@ -19,46 +17,7 @@ const Checkout = async ({
   if (!session) {
     redirect(`/login?${sParams}`);
   }
-  return (
-    <div className="flex container gap-6 mt-16 mx-auto">
-      <Card className="w-3/5 border-none">
-        <CardHeader>
-          <CardTitle>Customer details</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-6">
-            <div className="grid gap-3">
-              <Label htmlFor="fname">First Name</Label>
-              <Input
-                id="fname"
-                type="text"
-                className="w-full"
-                defaultValue=""
-              />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="lname">Last Name</Label>
-              <Input
-                id="lname"
-                type="text"
-                className="w-full"
-                defaultValue=""
-              />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="text"
-                className="w-full"
-                defaultValue=""
-              />
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  return <CustomerForm />;
 };
 
 export default Checkout;
