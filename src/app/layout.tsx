b622dd4ffb48e21d47277c9404cb3471f431ea75
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-24 21:00:56 +0530

    add tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/custom/header";
 import StoreProvider from "./StoreProvider";
 import { Toaster } from "sonner";
 import Refresher from "@/components/custom/refresher";
+import QueryProvider from "./QueryProvider";
 
 const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
@@ -28,11 +29,13 @@ export default function RootLayout({
             manrope.variable
           )}
         >
-          <Refresher>
-            <Header />
-            <main>{children}</main>
-            <Toaster />
-          </Refresher>
+          <QueryProvider>
+            <Refresher>
+              <Header />
+              <main>{children}</main>
+              <Toaster />
+            </Refresher>
+          </QueryProvider>
         </body>
       </StoreProvider>
     </html>
